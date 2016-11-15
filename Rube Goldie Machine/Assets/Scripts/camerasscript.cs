57cbf943e51ccc82c042d971f15831be518d8844
marrysoegino <ms8169@nyu.edu>
2016-11-15 16:13:49 -0500

    no message

--- Rube Goldie Machine/Assets/Scripts/camerasscript.cs
+++ Rube Goldie Machine/Assets/Scripts/camerasscript.cs
@@ -1,15 +1,63 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class camerasscript : MonoBehaviour {
+public class camerasscript : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public GameObject[] Cameras;
+    private int currentCam;
+
+
+    // Use this for initialization
+    void Start()
+    {
+      
+
+        DeactivateCameras();
+        Cameras[0].SetActive(true);
+        currentCam = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+           
+
+            DeactivateCameras();
+
+            if (currentCam < Cameras.Length - 1)
+            {
+                currentCam += 1;
+            }
+            else
+            {
+                currentCam = 0;
+            }
+
+
+            Cameras[currentCam].SetActive(true);
+
+        }
+
+    }
+
+    public void activateCamera(int _camNumber)
+    {
+
+        Cameras[_camNumber - 1].SetActive(true);
+        currentCam = _camNumber - 1;
+    }
+
+    public void DeactivateCameras()
+    {
+        for (int i = 0; i < Cameras.Length; i++)
+        {
+            Cameras[i].SetActive(false);
+        }
+
+    }
 }
---
+++ Rube Goldie Machine/Assets/Scripts/cameratrigger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameratrigger : MonoBehaviour
+{
+
+    //public GameObject camToActivate;
+    public int camNumber;
+
+    public camerasscript controller;
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        controller.DeactivateCameras();
+        controller.activateCamera(camNumber);
+
+    }
+}
