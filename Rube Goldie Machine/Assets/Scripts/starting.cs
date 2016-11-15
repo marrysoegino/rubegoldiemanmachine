fecbdbf6e8023ed8eac600b44239d60d61637cb5
marrysoegino <ms8169@nyu.edu>
2016-11-15 13:54:52 -0500

    no message

---
+++ Rube Goldie Machine/Assets/Scripts/starting.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class starting : MonoBehaviour {
+    private Transform myTransform;
+
+    public float moveSpeed;
+
+
+    // Use this for initialization
+    void Start () {
+        myTransform = GetComponent<Transform>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            myTransform.position += transform.right * Time.deltaTime * moveSpeed;
+        }
+
+    }
+}
